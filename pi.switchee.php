b9288c9716d1a5f5baf8383b84358f9a67dff97f
croxton <mcroxton@hallmark-design.co.uk>
2010-10-12 18:55:26 +0100

    No change

--- pi.switchee.php
+++ pi.switchee.php
@@ -2,7 +2,7 @@
 
 $plugin_info = array(
   'pi_name' => 'Switchee',
-  'pi_version' =>'1.5',
+  'pi_version' =>'1.4',
   'pi_author' =>'Mark Croxton',
   'pi_author_url' => 'http://www.hallmark-design.co.uk/',
   'pi_description' => 'Switch/case control structure for templates',
@@ -24,28 +24,20 @@ class Switchee {
 	 */
 	public function Switchee() 
 	{
-		global $TMPL, $REGX, $IN;
+		$this->EE =& get_instance();
 		
 		// the variable we want to find
-		$var = $TMPL->fetch_param('variable') ? $TMPL->fetch_param('variable') : '';
-		
-		// register POST and GET values
-		if (strncmp($var, 'get:', 4) == 0)
-		{
-			$var = filter_var($IN->GBL(substr($var, 4), 'GET'), FILTER_SANITIZE_STRING);
-		}
-		
-		if (strncmp($var, 'post:', 5) == 0)
-		{
-			$var = filter_var($IN->GBL(substr($var, 5), 'POST'), FILTER_SANITIZE_STRING);
-		}
+		$var = $this->EE->TMPL->fetch_param('variable') ? $this->EE->TMPL->fetch_param('variable') : '';
 		
 		// fetch the tagdata
-		$tagdata = $TMPL->tagdata;
-			
+		$tagdata = $this->EE->TMPL->tagdata;
+		
 		// loop through case parameters and find a case pair value that matches our variable
 		$index = 0;
-		foreach ($TMPL->var_pair as $key => $val)
+		
+		$this->return_data = '';
+		
+		foreach ($this->EE->TMPL->var_pair as $key => $val)
 		{
 			// is this tag pair a case?
 			if (preg_match('/^case/', $key))
@@ -55,7 +47,7 @@ class Switchee {
 					
 				// define the pattern we're searching for in tagdata that encloses the current case content
 				// make search string safe by replacing any regex in the case values with a marker
-				$pattern = '/{case_'.$index.'}(.*){&#47;case}/Usi';
+				$pattern = '/{case_'.$index.'}(.*){\/case}/Usi';
 				$tagdata = str_replace($key, 'case_'.$index, $tagdata);
 				
 				if(isset($val['value']))
@@ -81,8 +73,8 @@ class Switchee {
 						}
 						
 						// decode any encoded characters
-						$case_value = $REGX->unhtmlentities($case_value);
-						$var 		= $REGX->unhtmlentities($var);
+						$case_value = $this->EE->security->entity_decode($case_value);
+						$var 		= $this->EE->security->entity_decode($var);
 
 						// is the case value a regular expression?
 						// check for a string contained within hashes #regex#
@@ -120,9 +112,6 @@ class Switchee {
 				}	
 			}
 		}
-		
-		// replace namespaced no_results with the real deal
-		$this->return_data = str_replace(strtolower(__CLASS__).'_no_results', 'no_results', $this->return_data);
 	}
 
 	// usage instructions
@@ -153,17 +142,6 @@ HOW TO USE
 	{/case}
 	
 {/exp:switchee}
-
-
-How to support no_result blocks inside wrapped tags:
-
-{if switchee_no_results}
-	{redirect="channel/noresult"}
-{/if}
-
-GET and POST globals can also be evaluated by prefixing with get: or post:, e.g.:
-{exp:switchee variable = "post:my_var" parse="inward"}
-
 
 Requires PHP 5.
 
